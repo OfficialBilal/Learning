202e7295e3252c2bf2952ea9df4071b897a2efb5
OfficialBilal <chbilal.official@gmail.com>
2024-12-24 16:21:02 +0500

    Arithmetic Oprators

---
+++ Learning/ArithmeticOprators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class ArithmeticOprators
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("10 + 5 = {0}" ,10 + 5);
+            Console.WriteLine("10 - 5 = {0}" ,10 - 5);
+            Console.WriteLine("10 / 5 = {0}" ,10 / 5);
+            Console.WriteLine("10 * 3 = {0}" ,10 * 5);
+            Console.WriteLine("10 % 5 = {0}" ,11 % 5);
+        }                             
+    }
+}
