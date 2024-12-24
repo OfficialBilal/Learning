56bb414d874cc45f2e76f65a9db782f13bc8cd0e
OfficialBilal <chbilal.official@gmail.com>
2024-12-24 17:51:11 +0500

    Type Casting

---
+++ Learning/TypeCasting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class TypeCasting
+    {
+       public static void Main(string[] args)
+       {
+          // Converting one type to another type
+
+          decimal d = 0.01m;
+
+          bool i = Convert.ToBoolean(d);
+
+          Console.WriteLine(i);
+       }
+    }
+}
+
