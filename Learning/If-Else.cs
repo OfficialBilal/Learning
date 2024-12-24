16000d33d8f2d8b16e0d70a677011a9e3d2add4f
OfficialBilal <chbilal.official@gmail.com>
2024-12-24 19:54:52 +0500

    If else

---
+++ Learning/If-Else.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class If_Else
+    {
+        public static void Main(string[] args)
+        {
+            int grades = 46;
+            #pragma warning disable
+            bool HasPassed = false;
+
+            if (grades > 45)
+            {
+                Console.WriteLine("You are Passed");
+            }
+            else if (grades > 30)
+            {
+                Console.WriteLine("You are mar mar ky Passed");
+            }
+            else
+            {
+                Console.WriteLine("You are Failed");
+            }
+
+        }
+    }
+}
