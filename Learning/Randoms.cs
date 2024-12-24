17df3b112edcf146d3dc175006270e7bb1568422
OfficialBilal <chbilal.official@gmail.com>
2024-12-24 17:18:23 +0500

    Random

---
+++ Learning/Randoms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Randoms
+    {
+        public static void Main(string[] args)
+        {
+            Random r = new();
+
+            Console.WriteLine(r.Next(0,11));
+            Console.WriteLine(r.NextDouble());
+            Console.WriteLine(r.NextSingle());
+        }
+    }
+}
