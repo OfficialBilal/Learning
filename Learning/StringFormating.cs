dd4950b884140fa803503f08fccc6d1093999679
OfficialBilal <chbilal.official@gmail.com>
2024-12-24 14:23:25 +0500

    Add project files.

---
+++ Learning/StringFormating.cs
@@ -0,0 +1,18 @@
+﻿namespace Learning
+{
+    internal class StringFormating
+    {
+        static void Main(string[] args)
+        {
+            int age = 26;
+            string name = "Bilal";
+            decimal price = 12090.3563452m;
+
+            string str = string.Format("Hi, {0} you are {1} years old", name, age);
+            string pricee = string.Format("Total Price: {0:c}", price);
+
+            Console.WriteLine(str);
+            Console.WriteLine(pricee);
+        }
+    }
+}
