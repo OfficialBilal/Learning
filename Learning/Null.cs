ae6a58c39f3c15ffe7698245946d3053222adbc9
OfficialBilal <chbilal.official@gmail.com>
2024-12-24 20:20:16 +0500

    Null

---
+++ Learning/Null.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Null
+    {
+        public static void Main(string[] args)
+        {
+            string name1 = null;
+            string other1 = name1 ?? "Unknown";
+            Console.WriteLine(other1);
+
+            string name2 = "Bilal";
+            string other2 = name2 ?? "Unknown";
+            Console.WriteLine(other2);
+
+            int? age = null;
+            int? a = age ?? 10;
+            Console.WriteLine(a);
+        }
+    }
+}
