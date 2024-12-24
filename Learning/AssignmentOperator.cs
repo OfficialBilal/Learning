6aa072e31b63582303d6bb7f69561ad38896d085
OfficialBilal <chbilal.official@gmail.com>
2024-12-24 17:44:28 +0500

    Assignment Operator

---
+++ Learning/AssignmentOperator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class AssignmentOperator
+    {
+        public static void Main(string[] args)
+        {
+            int number, other;
+
+            number = 5;
+            number = other = 30;
+
+            number += 10;
+            number += other += 20;
+
+            Console.WriteLine(number);
+        }
+    }
+}
