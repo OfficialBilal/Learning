af70ed96482249adb1ae438a9e457446604d9bbe
OfficialBilal <chbilal.official@gmail.com>
2024-12-24 16:34:06 +0500

    Logical Operator

---
+++ Learning/LogicalOperator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class LogicalOperator
+    {
+        public static void Main(string[] args)
+        {
+            bool a = true && true;
+            bool b = true && false;
+            bool c = true || true;
+            bool d = true || false;
+            bool e = false || false;
+
+            bool f = false || true;
+            bool g = !f;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+        }
+    }
+}
