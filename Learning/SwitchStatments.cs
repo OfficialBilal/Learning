f3b28d80dec14d9e0d318544f42232d7850bf91e
OfficialBilal <chbilal.official@gmail.com>
2024-12-25 13:55:10 +0500

    Switch Statments

---
+++ Learning/SwitchStatments.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class SwitchStatments
+    {
+        public static void Main(string[] args)
+        {
+            object command = 43.9f;
+
+            switch (command)
+            {
+                case int:
+                    Console.WriteLine("Integer");
+                     break;
+
+                case string:
+                    Console.WriteLine("String");
+                    break;
+
+                case float:
+                    Console.WriteLine("Float");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Command");
+                    break;
+
+            }
+        }
+    }
+}
+ 
