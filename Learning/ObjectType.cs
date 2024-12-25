123772383dbefe387859e6ae361ca447b7081ea2
OfficialBilal <chbilal.official@gmail.com>
2024-12-25 13:36:08 +0500

    Object Type

---
+++ Learning/ObjectType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class ObjectType
+    {
+        public static void Main(string[] args)
+        {
+            object obj = "Hello, How are you?";
+            obj = 2;
+            obj = 4.565f;
+            Console.WriteLine(obj);
+
+            if (obj is int)
+            {
+                //int number = (int)obj;
+                Console.WriteLine("Interger");
+            }
+            else if (obj is string)
+            {
+               Console.WriteLine("String");
+            }
+            else if (obj is float)
+            {
+                Console.WriteLine("Float");
+            }
+        }
+    }
+}   
