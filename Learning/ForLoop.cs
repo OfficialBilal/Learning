ae8a2ca0b7295621cca980415f0f81f8045983e0
OfficialBilal <chbilal.official@gmail.com>
2024-12-25 15:14:03 +0500

    For Loop

---
+++ Learning/ForLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class ForLoop
+    {
+        public static void Main(string[] args)
+        {
+            for ( int i = 0 ; i < 10 ; i++ )
+            {
+                for (int j = i ; j < 10 ; j++ )
+                {
+                    Console.Write(i+" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
