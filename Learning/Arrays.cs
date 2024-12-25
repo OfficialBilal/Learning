b78e2ce4701e86f50ae4ce3e9a37b159d8f17a7e
OfficialBilal <chbilal.official@gmail.com>
2024-12-25 15:46:20 +0500

    Arrays

---
+++ Learning/Arrays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Arrays
+    {
+        public static void Main(string[] args)
+        {
+            int[] number = new int[5] { 1, 2, 3, 4, 5 };
+            object[] obj = { new(), new(), new(), new()};
+
+            obj[0] = 12;
+            obj[0] = "Bilal";
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine(number[i]);
+            }
+
+            Console.WriteLine(obj[0]);
+        }
+    }
+}
