88c8d34c9de0748ea5989bed0bc3d415addba5e2
OfficialBilal <chbilal.official@gmail.com>
2024-12-25 15:55:10 +0500

    Var Keyword

---
+++ Learning/Var.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Var
+    {
+        public static void Main(string[] args)
+        { 
+            object obj = "Bilal";
+
+            var number = obj;
+
+            Console.WriteLine(number);
+        }
+    }
+}
