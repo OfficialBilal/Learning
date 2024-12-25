b72c2301093216741481239e20d64781d50c8b28
OfficialBilal <chbilal.official@gmail.com>
2024-12-25 14:18:31 +0500

    While - DoWhile

---
+++ Learning/While-DoWhile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class While_DoWhile
+    {
+        public static void Main(string[] args)
+        {
+            int Counter = 0;
+            
+            while (Counter < 50)
+            {
+                Console.WriteLine($"{Counter} Bilal");
+                Counter++;
+            }
+
+            Console.WriteLine("Switching to Do-While Loop");
+
+            do
+            {
+                Console.WriteLine($"{Counter} Bilal");
+                Counter++;
+
+            } while (Counter < 100);
+        }
+    }
+}
