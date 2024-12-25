04f5ec1cab0fc5b6a6e3173b35a3d953ada4c47a
OfficialBilal <chbilal.official@gmail.com>
2024-12-25 16:49:51 +0500

    Multi Dimensional Array

---
+++ Learning/MD-Array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class MD_Array
+    {
+        public static void Main(string[] args)
+        {
+            int[,] num =
+
+                { { 1, 2, 3},
+                  { 4, 5, 6},
+                  { 7, 8, 9},};
+
+            num[1, 2] = 10;
+             
+             Console.WriteLine(num[1, 2]);
+
+            int[,] a = new int[9, 7];
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    a[i, j] = i+j;
+                    Console.Write(a[i, j]+ "");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
