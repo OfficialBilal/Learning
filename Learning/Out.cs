f14f1f51f309cfc8ae8b31048da7950b960b1b59
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 14:29:58 +0500

    Out

---
+++ Learning/Out.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Out
+    {
+        public static void Main(string[] args)
+        {
+            New(out int number);
+
+            Console.WriteLine(number);
+        }
+
+        static void New(out int n)
+        {
+            n = 10;
+        }
+    }
+}
