f368fb96b2e56ed41b0fcdbdb00357c83a59f351
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 22:00:08 +0500

    Classes Extra

---
+++ Learning/ClassesExtra.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+     class ClassesExtra
+    {
+        public static void Main(string[] args)
+        {
+            LinkedClass person = new LinkedClass()
+            {
+                Name = "Bilal",
+                Age = 25
+            };
+
+            person.Talk();
+
+        }
+
+    }
+}
