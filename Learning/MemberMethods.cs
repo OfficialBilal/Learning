7e3f3cac412944c108606771f33d955032658f50
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 21:46:45 +0500

    Member Methods

---
+++ Learning/MemberMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class MemberMethods
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "Bilal";
+
+            person.Age = 25;
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+
+            person.Talk();
+
+        }
+
+        class Person
+        {
+            public string Name = "";
+
+            public int Age;
+
+            public void Talk()
+            {
+                Console.WriteLine("Yeayae");
+            }
+        }
+    }
+}
