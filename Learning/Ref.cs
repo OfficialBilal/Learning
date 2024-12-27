33f96ba91468fd2d8bea2ea8d63d42d999c86c8e
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 14:03:28 +0500

    References

---
+++ Learning/Ref.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Ref
+    {
+        public static void Main(string[] args)
+        {
+            int num = 10;
+            ref int a = ref num;
+            a = 12;
+            Console.WriteLine(num);
+
+
+            int b = 20;
+            ref int r = ref b;
+            A(ref r);
+        }
+
+        static void A(ref int r)
+        {
+            Console.WriteLine(r);
+        }
+    }
+}
