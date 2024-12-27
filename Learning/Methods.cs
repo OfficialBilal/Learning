01ef26f6802603d54524431f9dc681287c0d8863
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 10:35:50 +0500

    Methods

---
+++ Learning/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Methods
+    {
+        //Method/Function
+        public static void Main(string[] args)
+        {
+            NewFunction();
+        }
+
+        static void NewFunction()
+        {
+            Console.WriteLine("I am from a new Funtion/Method");
+        }
+    }
+}
