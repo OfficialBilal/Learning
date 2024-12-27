e022da167e350895c31cec5ad36f3b7e2ec4b819
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 21:20:46 +0500

    Conditional Operator

---
+++ Learning/ConditionalOperator.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Cache;
+
+namespace Learning
+{
+    class ConditionalOperator
+    {
+        public static void Main(string[] args)
+        {
+            bool CanVote = false;
+
+            int Age = 18;
+
+            string Vote;
+
+            CanVote = Age >= 18 ? true : false;
+
+            Vote = CanVote ? "You are allowed to Vote" : "You are not allowed to vote";
+
+            Console.WriteLine(Vote);
+        } 
+    }
+}
