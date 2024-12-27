249f9194c8b187cfcd92704883ed2bb276619ce2
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 21:35:59 +0500

    Classes

---
+++ Learning/Classes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Classes
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "Bilal";
+
+            person.Age = 25;
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+
+        }
+
+        class Person
+        {
+            public string Name = "";
+
+            public int Age;
+        }
+    }
+}
