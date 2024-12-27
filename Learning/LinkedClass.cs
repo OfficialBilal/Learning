3ad89360a749834c601bff72a8a214296499d8dc
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 21:59:48 +0500

    Linked Class

---
+++ Learning/LinkedClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class LinkedClass
+    {
+        public string Name = "";
+
+        public int Age;
+
+        public void Talk()
+        {
+            Console.WriteLine("Ali is Yeayae-ing and is {0} Year old", Age);
+        }
+    }
+}
