c906e579ee175d6c735c8d45ae4bf88e263f8730
OfficialBilal <chbilal.official@gmail.com>
2024-12-27 14:19:43 +0500

    In

---
+++ Learning/In.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class In
+    {
+        public static void Main(string[] args)
+        {
+            int number = 8;
+
+            New(number);
+        }
+
+        static void New(in int a)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
