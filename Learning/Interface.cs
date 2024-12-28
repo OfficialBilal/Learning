01753bc0d9b6ad3b8f361ab32d2cf993cd4482ce
OfficialBilal <chbilal.official@gmail.com>
2024-12-28 16:00:24 +0500

    Interface

---
+++ Learning/Interface.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    interface IPerson
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+
+        string GetName();
+    }
+
+    interface IManager
+    {
+        string Role { get; set; }
+    }
+
+    class Employee : IPerson, IManager
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Role { get; set; }
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+    class Interface
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Name = "Bilal from Interface";
+            Console.WriteLine(emp.Name);
+        }
+    }
+}
