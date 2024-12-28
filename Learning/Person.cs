5895474ccfee765a1e3bbc249af04e76ff41ca68
OfficialBilal <chbilal.official@gmail.com>
2024-12-28 15:22:35 +0500

    Abstract

--- Learning/Person.cs
+++ Learning/Person.cs
@@ -6,25 +6,29 @@ using System.Threading.Tasks;
 
 namespace Learning
 {
-    class Person
+    abstract class Person
     {
         public string Name = string.Empty;
 
         public byte Age = 0;
 
-        public void Talk()
-        {
-            Console.WriteLine("Person Can Talk");
-        }
+        abstract public void Talk();
+        
     }
 
     class Doctor : Person
     {
-
-    }
+        public override void Talk()
+        {
+            Console.WriteLine("Person Can Talk");
+        }
+}
 
     class Patient : Person
     {
-
+        public override void Talk()
+        {
+            Console.WriteLine("Person Can Talk");
+        }
     }
 }
