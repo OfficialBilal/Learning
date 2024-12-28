6b194950d7d79dc6cb7c26e8d04eb2f448c4a6ee
OfficialBilal <chbilal.official@gmail.com>
2024-12-28 17:02:38 +0500

    Delegates

---
+++ Learning/Delegates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Delegates
+    {
+        static void AlertPhone()
+        {
+            Console.WriteLine("Phoning...");
+        }
+
+        static void AlertDesktop()
+        {
+            Console.WriteLine("Desktoping...");
+        }
+
+        static void AlertTablet()
+        {
+            Console.WriteLine("Tableting...");
+        }
+
+        delegate void Alert();
+        public static void Main(string[] args)
+        {
+           Alert alert = AlertPhone;
+           alert += AlertDesktop;
+           alert += AlertTablet;
+
+            alert();
+
+
+        }
+    }
+}
