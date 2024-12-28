97be07bcef14a186d6428dfa943db8ead842a73b
OfficialBilal <chbilal.official@gmail.com>
2024-12-28 15:11:01 +0500

    Inheritance

---
+++ Learning/Inheritance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Inheritance
+    {
+        public static void Main(string[] args)
+        {
+            Doctor doctor = new();
+            doctor.Talk();
+        }
+    }
+}
