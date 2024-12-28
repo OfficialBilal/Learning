fb62f7b3f348f6fef9f614101af2f9605488f117
OfficialBilal <chbilal.official@gmail.com>
2024-12-28 16:29:00 +0500

    Polymorphism

---
+++ Learning/Polymorphism.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Polymorphism
+    {
+        interface IPerson
+        {
+            string? Who();
+        }
+
+        class Employee : IPerson
+        {
+            public string? Who()
+            {
+                return ToString()?.Split('.')[^1];
+            }
+        }
+
+        class Manager : IPerson
+        {
+            public string? Who()
+            {
+                return ToString()?.Split('.')[^1];
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            IPerson person = new Employee();
+            person = new Manager();
+            Console.WriteLine(person.Who());
+        }
+    }
+}
