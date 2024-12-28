1700519e458bb1c64b02c1a0dd7cc52ad753746b
OfficialBilal <chbilal.official@gmail.com>
2024-12-28 15:12:12 +0500

    Getter-Setter

---
+++ Learning/Getters-Setters.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Learning
+{
+    class Getters_Setters
+    {
+        static void Main(string[] args)
+        {
+            Person1 person = new Person1();
+
+            person.Name = "Bilal";
+            person.Age = 18;
+
+            Console.WriteLine($"Name: {person.Name}");
+
+            
+
+        }
+    }
+    class Person1
+    {
+        private string name = string.Empty;
+        private int age = 19;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value <= 18)
+                { 
+                    Console.WriteLine("You must be 18 Year old to Vote");
+                return;
+                }
+                age = value;
+            }
+        }
+    }
+}
---
+++ Learning/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Person
+    {
+        public string Name = string.Empty;
+
+        public byte Age = 0;
+
+        public void Talk()
+        {
+            Console.WriteLine("Person Can Talk");
+        }
+    }
+
+    class Doctor : Person
+    {
+
+    }
+
+    class Patient : Person
+    {
+
+    }
+}
