114adf6d9edf65a355e69f670c0acecfa8b495dc
OfficialBilal <chbilal.official@gmail.com>
2024-12-28 15:11:47 +0500

    Constructor

---
+++ Learning/Constructors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Constructors
+    {
+        public static void Main(string[] args)
+        {
+            Person1 person = new Person1()
+            {
+                Name = "Test",
+                Age = 1
+            };
+            Console.WriteLine(person.Name);
+        }
+    }
+}
