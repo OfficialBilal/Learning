536ea1331111241de4a17a0f36262c8599a66434
OfficialBilal <chbilal.official@gmail.com>
2024-12-29 17:58:43 +0500

    Generics

---
+++ Learning/Generics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Generics
+    {
+        public static void Main(string[] args)
+        {
+            T Doubleit<T>(T number)
+            {
+                return (dynamic)number * 2;
+            }
+
+            var twice = Doubleit(34.34);
+            Console.WriteLine(twice);
+        }
+    }
+}
