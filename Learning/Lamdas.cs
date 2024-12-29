c291c26b6eedc10ca9fb1defacb789011f084707
OfficialBilal <chbilal.official@gmail.com>
2024-12-29 19:04:59 +0500

    Lamdas

---
+++ Learning/Lamdas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Lamdas
+    {
+        public static void Main(string[] args)
+        {
+            var isEven = (int n) => n % 2 == 0;
+
+            Console.WriteLine(isEven(8));
+        }
+    }
+}
